52b97e164365a38d74772a3195fe17bf70c9775b
laddieri <laddieri@gmail.com>
2020-10-19 19:59:15 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,13 +24,66 @@ class Ball {
     stroke(0,0,0);
     fill('rgb(241, 194, 125)');
     ellipse(this.x,100,this.radius,this.radius);
-    ellipse(this.x+30*this.direction,55,this.radius/2.5,this.radius/8);
+    ellipse(this.x+30*this.direction,55,this.radius/2.5,this.radius/8)
     ellipse(this.x+50*this.direction,75,this.radius/2.5,this.radius/8);
     ellipse(this.x+50*this.direction,95,this.radius/2.5,this.radius/8);
     ellipse(this.x+50*this.direction,115,this.radius/2.5,this.radius/8);
     ellipse(this.x+50*this.direction,135,this.radius/2.5,this.radius/8);
   }
 
+}
+
+class Pig {
+  constructor(direction){
+    this.direction=direction;
+    this.x = 100;
+  }
+
+  pigmove(){
+    this.x = this.direction*500/(secondsPerBeat*60)*(t-((1/(secondsPerBeat*60))*t*t))+(640/2);
+    console.log(this.x)
+  }
+
+  display(){
+      var bodyX = this.x; // variabels
+      var bodyY = 200;
+      fill(250, 192, 196); //legs
+    rect(bodyX+18, bodyY+73, 18, 68);
+    rect(bodyX-47, bodyY+71, 18, 68);
+
+    ellipse(bodyX, bodyY, 245, 245); // body
+
+    fill(163, 124, 127);//lefteare
+    triangle(bodyX, bodyY-54, bodyX-70, bodyY+15, bodyX-66,bodyY-87);
+
+    //rightears
+    triangle(bodyX+65, bodyY+24, bodyX+70, bodyY-85, bodyX-8,bodyY-58);
+    fill(13, 13, 13); // earefill
+
+    triangle(bodyX+26, bodyY+62, bodyX+65, bodyY-77, bodyX-38,bodyY-24); // earefill
+    triangle(bodyX, bodyY-31, bodyX-52, bodyY+38, bodyX-59,bodyY-77);
+    fill(217, 165, 169);
+    ellipse(bodyX, bodyY, 155, 144); //head
+
+    fill(224, 107, 117); //nose
+    ellipse(bodyX, bodyY+13, 71, 60);
+
+    fill(0, 0, 0); //nosefill
+    ellipse(bodyX-10, bodyY+12, 11, 19);
+    ellipse(bodyX+10, bodyY+12, 11, 19);
+
+    ellipse(bodyX-17, bodyY-24, 6, 15); //pupils
+    ellipse(bodyX+17, bodyY-24, 6, 15);
+
+    fill(259, 192, 196); //legs
+    rect(bodyX-81, bodyY+78, 18, 68);
+    rect(bodyX+51, bodyY+72, 18, 68);
+    fill(8, 8, 8);
+    ellipse(bodyX-72, bodyY+141, 21, 11);
+    ellipse(bodyX+60, bodyY+141, 21, 11);
+    ellipse(bodyX-38, bodyY+138, 18, 10);
+    ellipse(bodyX+28, bodyY+138, 18, 10);
+  }
 }
 
 // Start Audio Context on Mouseclick
@@ -94,8 +147,11 @@ function setup() {
 
 
   // Create 2 instances of ball class
-  ball1 = new Ball(100, 1);
-  ball2 = new Ball(100,-1);
+  // ball1 = new Ball(100, 1);
+  // ball2 = new Ball(100,-1);
+
+  pig1 = new Pig(1);
+  pig2 = new Pig(-1)
 }
 
 
@@ -106,11 +162,18 @@ function draw() {
       background('#696969');
   }
 
+
   t++;
-  ball1.display();
-  ball1.ballmove();
-  ball2.display();
-  ball2.ballmove();
+  // ball1.display();
+  // ball1.ballmove();
+  // ball2.display();
+  // ball2.ballmove();
+  pig1.display();
+  pig1.pigmove();
+  pig2.display();
+  pig2.pigmove();
 }
+
+
 
 updateTime()
